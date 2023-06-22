10ab7be712abd2022443ee22c34563f11946c1a3
Alison Souza <alisonmsouza@yahoo.com.br>
2023-06-22 14:04:09 -0300

    lambda and api gateway

---
+++ lambda-api-gateway/infra/main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "lambda_root" {
+  type        = string
+  description = "The relative path to the source of the lambda"
+  default     = "../src"
+}
+
+resource "null_resource" "install_dependencies" {
+  provisioner "local-exec" {
+    command = "pip install -r ${var.lambda_root}/requirements.txt -t ${var.lambda_root}/"
+  }
+
+  triggers = {
+    dependencies_versions = filemd5("${var.lambda_root}/requirements.txt")
+    source_versions = filemd5("${var.lambda_root}/main.py")
+  }
+}
+
+provider "archive" {}
+data "archive_file" "zip" {
+  type        = "zip"
+  source_dir  = "${var.lambda_root}"
+  output_path = "../output/currency.zip"
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    sid    = ""
+    effect = "Allow"
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.policy.json
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name    = "convert_currency"
+  filename         = data.archive_file.zip.output_path
+  source_code_hash = data.archive_file.zip.output_base64sha256
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "main.lambda_handler"
+  runtime          = "python3.9"
+}
+
